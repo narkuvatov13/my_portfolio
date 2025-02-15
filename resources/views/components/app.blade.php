fe0cf2ac0b88e15797128dcd43599203877c3ce7
Yusup Narkuvatov <ynn.131095@gmail.com>
2025-02-15 22:19:17 +0300

    Added Alpinejs

---
+++ resources/views/components/app.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>{{ env('APP_NAME') }}</title>
+
+    <!-- AlpineJs -->
+    <!-- <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script> -->
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <!-- Styles / Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+</head>
+
+<body class="font-sans antialiased dark:bg-black dark:text-white/50">
+
+    <x-navbar></x-navbar>
+
+    {{ $slot }}
+
+    <x-frontend-layout>
+        Test
+    </x-frontend-layout>
+
+</body>
+
+</html>
---
+++ resources/views/components/frontend-layout.blade.php
@@ -0,0 +1,5 @@
+<div class="">
+    <h1 class="text-3xl font-bold  text-purple-500 underline">
+        {{ $slot }}
+    </h1>
+</div>
